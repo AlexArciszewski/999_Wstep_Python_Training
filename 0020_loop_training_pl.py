4c160ce7ea4c82e61675d10c2e3df06fea4502ae
AlexArciszewski <102918054+AlexArciszewski@users.noreply.github.com>
2022-06-17 12:49:55 +0200

    Rename liczba petla100_0.py to 0020_loop_training_pl.py

---
+++ 0020_loop_training_pl.py
@@ -0,0 +1,17 @@
+# -*- coding: cp1250 -*-
+#Pętla While
+
+#suma = 0
+#x = int(input("Podaj kolejnš liczbę"))
+
+
+#liczba = 0      poczštkowa liczba while jest <=5 podaj liczbę i zwiększ o 1
+#while liczba <=5:
+ #   print(liczba)
+  #  liczba +=1
+
+liczba = 100                  #liczba poczatkowa
+while liczba >=0:             #podczas gdy liczba >=liczba końcowa
+    print(liczba)             #podaj każdš liczbę
+    liczba -=1                #liczba=liczba -1 czyli kolejna jest o 1 mniejsza od nastepnej
+
